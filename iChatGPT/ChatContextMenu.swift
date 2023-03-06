09ee8db51504adec8b5d09789d75664764f95b07
AlphaGogoo <1411420236@qq.com>
2023-03-06 22:46:31 +0800

    简化iChatGPT的使用方法

--- iChatGPT/ChatContextMenu.swift
+++ iChatGPT/ChatContextMenu.swift
@@ -13,7 +13,7 @@ struct ChatContextMenu: View {
     @Binding var searchText: String
     @StateObject var chatModel: AIChatModel
     let item: AIChat
-    
+//
     var body: some View {
         VStack {
             CreateMenuItem(text: "重新提问", imgName: "arrow.up.message") {
@@ -22,21 +22,21 @@ struct ChatContextMenu: View {
             CreateMenuItem(text: "复制问题", imgName: "doc.on.doc") {
                 item.issue.copyToClipboard()
             }
-            
+
             CreateMenuItem(text: "复制答案", imgName: "doc.on.doc") {
                 item.answer!.copyToClipboard()
             }
             .disabled(item.answer == nil)
-            
+
             CreateMenuItem(text: "复制问题和答案", imgName: "doc.on.doc.fill") {
                 "\(item.issue)\n-----------\n\(item.answer ?? "")".copyToClipboard()
             }
             .disabled(item.answer == nil)
-            
+
             CreateMenuItem(text: "复制问题到输入框", imgName: "keyboard.badge.ellipsis") {
                 searchText = item.issue
             }
-            
+
             // remove item
             let isWait = chatModel.contents.filter({ $0.isResponse == false })
             CreateMenuItem(text: "删除问题", imgName: "trash", isDestructive: true) {
@@ -47,7 +47,7 @@ struct ChatContextMenu: View {
             }.disabled(isWait.count > 0)
         }
     }
-    
+
     func CreateMenuItem(text: String, imgName: String, isDestructive: Bool = false, onAction: (() -> Void)?) -> some View {
         if #available(iOS 15.0, *) {
             return Button(role: isDestructive ? .destructive : nil) {
@@ -64,10 +64,3 @@ struct ChatContextMenu: View {
         }
     }
 }
-
-//struct ChatContextMenu_Previews: PreviewProvider {
-//    
-//    static var previews: some View {
-//        ChatContextMenu(item: AIChat(datetime: Date().currentDateString(), issue: "我是问题~", answer: "答案是我"), searchText: $searchText)
-//    }
-//}
